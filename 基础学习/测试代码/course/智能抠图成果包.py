a140fadfe36d9e7385a62171167d1850bb9a0a97
koolook <koolook_long@163.com>
2022-03-14 20:36:47 +0800

    测试

--- 基础学习/测试代码/course/智能抠图成果包.py
+++
@@ -1,9 +0,0 @@
-# 智能抠图
-# 引入removebg中的RemoveBg工具
-from removebg import RemoveBg
-
-# 调用RemoveBg工具创建抠图实例，将抠图实例重新命名为rmbg
-rmbg = RemoveBg("sMUAw4jKgQHaoyvsH4HKR4U3", "error.log")
-
-# 调用工具的rmbgremove_background_from_img_file()方法进行扣图
-rmbg.remove_background_from_img_file("meixi.jpg")
